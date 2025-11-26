c147ee6e7e9f82eca2d4cdc4ceabd772e0e72ae4
Mauro Villagra <maurovillagra257@gmail.com>
2025-11-25 23:25:07 -0300

    Conexion al back, para mas adelante

---
+++ src/backend/connections/urgenciasService.js
@@ -0,0 +1,13 @@
+// import { api } from "./HttpClient";
+
+// export const urgenciasService = {
+//   getPacientes: () => api.get("urgencias/pacientes"),
+
+//   crearPaciente: (data) => api.post("urgencias/pacientes", data),
+
+//   actualizarPaciente: (id, data) =>
+//     api.put(`urgencias/pacientes/${id}`, data),
+
+//   eliminarPaciente: (id) =>
+//     api.delete(`urgencias/pacientes/${id}`),
+// };
